12f0b5edf5475634d157ed3611da7548e3a50add
daniCodes1 <danirenn16@gmail.com>
2025-01-18 11:06:40 -0800

    Flask setup

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py
+from flask import Flask, render_template
+from flask import jsonify
+
+app = Flask(__name__)
+
+# Display your index page
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/concatenate")
+def concatenate():
+    # Could also just get it from frontend just an eg of routing
+    str1 = "Adrian"
+    str2 = "Michael"
+    str3 = "Rishabh"
+    result = str1 + " " + str2 + " " + str3
+    return jsonify({"result": result})
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080)    
