22747a992ad50bc9ba1ed0c93517285785d59f01
mvelve <mcvelve@gmail.com>
2025-01-18 16:42:31 -0800

    updated backend

--- backend/database_connection.py
+++ backend/database_connection.py
@@ -1,1 +1,15 @@
-print("hello world")
+from fastapi import Request, APIRouter
+from pymongo import mongo_client
+
+db_router = APIRouter()
+
+@db_router.post("/upload_db")
+def connect_to_db(request: Request):
+
+    query_params = request.query_params
+
+
+
+    return {"result": "successful"}
+    
+
